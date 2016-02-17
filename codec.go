832b42aa3af67fe3b83ebede875ed550a15702d4
Vibhav Pant <vibhavp@gmail.com>
2016-02-17 17:42:49 +0530

    Add docs for EncodingCodec(s)

--- codec.go
+++ codec.go
@@ -6,13 +6,15 @@ import (
 	"encoding/json"
 )
 
+//EncodingCodec implements marshaling and unmarshaling of seralized data.
 type EncodingCodec interface {
 	Marshal(interface{}) ([]byte, error)
 	Unmarshal([]byte, interface{}) error
 }
 
+//JSONCodec is an EncodingCodec implementation to send/receieve JSON data
+//over AMQP.
 type JSONCodec struct{}
-type GobCodec struct{}
 
 func (JSONCodec) Marshal(v interface{}) ([]byte, error) {
 	b, err := json.Marshal(v)
@@ -23,6 +25,10 @@ func (JSONCodec) Unmarshal(data []byte, v interface{}) error {
 	err := json.Unmarshal(data, v)
 	return err
 }
+
+//GobCodec is an EncodingCodec implementation to send/recieve Gob data
+//over AMQP.
+type GobCodec struct{}
 
 func (GobCodec) Marshal(v interface{}) ([]byte, error) {
 	body := new(bytes.Buffer)
