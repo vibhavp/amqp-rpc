7cd24d29b14a1d59ca1bfa6c793d09b21a7e1b24
Vibhav Pant <vibhavp@gmail.com>
2016-02-17 18:47:49 +0530

    Add package doc

---
+++ doc.go
@@ -0,0 +1,4 @@
+//Package amqprpc is an AMQP driver for net/rpc. It provides an implementation
+//of the rpc.ClientCodec and rpc.ServerCodec interfaces, which send and respond to
+//RPC calls respecteively on a given AMQP connection.
+package amqprpc
